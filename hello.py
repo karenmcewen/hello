35cd90ad5ba83ada8bb9bbe2efaa00782e5657cb
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-02 23:21:36 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+print('hello world!')
+name=input('what is your name?')
+print('hello '+ name)
+year=int(input('what year were you born?'))
+age = 2018-year
+print('you are '+str(age)+' years old.')
+
